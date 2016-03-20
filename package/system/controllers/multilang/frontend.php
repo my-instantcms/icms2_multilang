2bddc13caacb9c93f1cc1718f40a6c72d336b8b2
My-InstantCMS.Ru <my-instantcms@mail.ru>
2016-03-20 19:19:55 +0400

    Проверка языка перед добавлением
    Проверка языка на существования, перед добавлением в сессию

--- package/system/controllers/multilang/frontend.php
+++ package/system/controllers/multilang/frontend.php
@@ -3,14 +3,21 @@
 class multilang extends cmsFrontend {
 	
 	public function actionSetlang($lang = false){
+		if(!$lang){cmsCore::error404();}
+		$langs = cmsCore::getLanguages();
+		$is_lang = in_array($lang, $langs);
+		if(!$is_lang){cmsCore::error404();}
 		$result = cmsUser::sessionSet('user:language', $lang);
 		$this->redirectBack();
 	}
 	
 	public function actionSessionlang(){
 		if(!$this->request->isAjax()){$this->cms_template->renderJSON(array('error'=>true));}
-		$lang = $this->request->get('lang');
+		$lang = $this->request->get('lang', '');
 		if(!$lang){$this->cms_template->renderJSON(array('error'=>true));}
+		$langs = cmsCore::getLanguages();
+		$is_lang = in_array($lang, $langs);
+		if(!$is_lang){$this->cms_template->renderJSON(array('error'=>true));}
 		$result = cmsUser::sessionSet('user:language', $lang);
 		$this->cms_template->renderJSON(array('error'=>$result));
 	}
