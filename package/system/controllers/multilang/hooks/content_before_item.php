bd875a1ffc3ced487899686cca7a80466ebf78f6
My-InstantCMS.Ru <my-instantcms@mail.ru>
2016-04-19 16:16:28 +0400

    SEO улучшения hreflang
    hreflang добавляется на все страницы
    исправлен баг для php7

--- package/system/controllers/multilang/hooks/content_before_item.php
+++ package/system/controllers/multilang/hooks/content_before_item.php
@@ -12,21 +12,6 @@ class onMultilangContentBeforeItem extends cmsAction {
 		if(isset($_SESSION['user']['language'])){
 			$user_lang = $_SESSION['user']['language'];
 		}
-		$this->model->filterEqual('item_id', $i['id'])->filterEqual('parent', $c['name'])->limit(15);
-		$translates = $this->model->selectOnly('i.id, i.lang')->get('multilang_contents');
-		if($translates){
-			$translates[0] = array('id' => 0, 'lang' => $this->cms_config->cfg_language);
-			$langs = cmsCore::getLanguages();
-			foreach($translates as $l){
-				$link = false;
-				if(in_array($l['lang'], $langs)){
-					$link = $this->cms_config->host. '/' . $l['lang'] . $_SERVER['REQUEST_URI'];
-				}
-				if($link){
-					$this->cms_template->addHead('<link rel="alternate" href="'.$link.'" hreflang="'.$l['lang'].'" />');
-				}
-			}
-		}
 		if($user_lang !== $this->cms_config->cfg_language){
 			$this->model->selectOnly('i.id, i.title, i.content');
 			$this->model->filterEqual('item_id', $i['id'])->filterEqual('parent', $c['name'])->filterEqual('lang', $user_lang);
--- package/system/controllers/multilang/hooks/engine_start.php
+++ package/system/controllers/multilang/hooks/engine_start.php
@@ -3,10 +3,19 @@
 class onMultilangEngineStart extends cmsAction {
     
     public function run(){
+		$langs = cmsCore::getLanguages();
+		if($langs){
+			foreach($langs as $lang){
+				$link = $this->cms_config->host. '/' . $lang . $_SERVER['REQUEST_URI'];
+				if($lang == $this->cms_config->cfg_language){
+					$link = $this->cms_config->host . $_SERVER['REQUEST_URI'];
+				}
+				$this->cms_template->addHead('<link rel="alternate" href="'.$link.'" hreflang="'.$lang.'" />');
+			}
+		}
 		$segments = explode('/', mb_substr($_SERVER['REQUEST_URI'], mb_strlen($this->cms_config->root)));
 		if (!empty($segments[0])) {
 			$may_be_lang = $segments[0];
-			$langs = cmsCore::getLanguages();
 			if(in_array($may_be_lang, $langs)){
 				cmsUser::sessionSet('user:language', $may_be_lang);
 				unset($segments[0]);
