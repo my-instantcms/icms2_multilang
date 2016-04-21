2eeec4cfdfbc92482494e13063e9b329d13c32f0
My-InstantCMS.Ru <my-instantcms@mail.ru>
2016-04-21 11:05:27 +0400

    Исправлен баг
    Баг с hreflang

--- package/system/controllers/multilang/hooks/engine_start.php
+++ package/system/controllers/multilang/hooks/engine_start.php
@@ -3,16 +3,7 @@
 class onMultilangEngineStart extends cmsAction {
     
     public function run(){
-		$langs = cmsCore::getLanguages();
-		if($langs){
-			foreach($langs as $lang){
-				$link = $this->cms_config->host. '/' . $lang . $_SERVER['REQUEST_URI'];
-				if($lang == $this->cms_config->cfg_language){
-					$link = $this->cms_config->host . $_SERVER['REQUEST_URI'];
-				}
-				$this->cms_template->addHead('<link rel="alternate" href="'.$link.'" hreflang="'.$lang.'" />');
-			}
-		}
+		$langs = cmsCore::getLanguages();		
 		$segments = explode('/', mb_substr($_SERVER['REQUEST_URI'], mb_strlen($this->cms_config->root)));
 		if (!empty($segments[0])) {
 			$may_be_lang = $segments[0];
@@ -20,6 +11,15 @@ class onMultilangEngineStart extends cmsAction {
 				cmsUser::sessionSet('user:language', $may_be_lang);
 				unset($segments[0]);
 				$_SERVER['REQUEST_URI'] = $this->cms_config->root.implode('/', $segments);
+			}
+		}
+		if($langs){
+			foreach($langs as $lang){
+				$link = $this->cms_config->host. '/' . $lang . $_SERVER['REQUEST_URI'];
+				if($lang == $this->cms_config->cfg_language){
+					$link = $this->cms_config->host . $_SERVER['REQUEST_URI'];
+				}
+				$this->cms_template->addHead('<link rel="alternate" href="'.$link.'" hreflang="'.$lang.'" />');
 			}
 		}
     }
