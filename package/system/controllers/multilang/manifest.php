a41709ec647aba5c599931b84586f35dc1b576bf
My-InstantCMS.Ru <my-instantcms@mail.ru>
2016-11-09 10:28:52 +0400

    Полное удаление
    Полное удаление для новой версии компонента

--- package/system/controllers/multilang/manifest.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	return array('hooks' => array(
-		'content_before_item',
-		'content_before_list',
-		'content_before_category',
-		'menu_before_list',
-		'widgets_before_list',
-		'engine_start',
-	));
