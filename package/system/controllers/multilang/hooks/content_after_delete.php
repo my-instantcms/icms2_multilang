3fd71f2d93d7af8b84d73299823469b51f8e6dc0
My-InstantCMS.Ru <my-instantcms@mail.ru>
2017-01-31 12:03:02 +0400

    Исправлен баг с удалением контента

--- package/system/controllers/multilang/hooks/content_after_delete.php
+++ package/system/controllers/multilang/hooks/content_after_delete.php
@@ -3,10 +3,8 @@
 class onMultilangContentAfterDelete extends cmsAction {
 
     public function run($data){
-		
-		list($ctype, $item) = $data;
 
-        $this->model->filterEqual('i.item_id', $item['id'])->deleteFiltered('multilang_con_' . $ctype['name']);
+        $this->model->filterEqual('i.item_id', $data['item']['id'])->deleteFiltered('multilang_con_' . $data['ctype_name']);
 
         return true;
 
